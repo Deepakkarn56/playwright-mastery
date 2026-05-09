d28c5b6274e356d0cfa04a27b8f654743a5f89a8
Deepakkarn56 <karndeepakk@gmail.com>
2026-05-10 00:22:11 +0530

    Added first Playwright test using codegen

---
+++ tests/record.test.js
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://github.com/Deepakkarn56');
+  await page.getByRole('link', { name: 'Sign in' }).click();
+  await page.getByRole('textbox', { name: 'Username or email address' }).click();
+  await page.getByRole('textbox', { name: 'Username or email address' }).fill('dvjhdfkv');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('12345');
+  await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+  await page.getByText('Incorrect username or').click();
+  await page.getByText('Incorrect username or').click();
+  await page.getByText('Incorrect username or').click();
+  await page.getByText('Incorrect username or').click();
+  await page.locator('div').filter({ hasText: 'Incorrect username or' }).nth(5).click();
+  await page.getByText('Incorrect username or').click();
+  await expect(page.getByRole('alert')).toContainText('Incorrect username or password.');
+  await page.locator('div').filter({ hasText: 'Incorrect username or' }).nth(5).click();
+  await expect(page.getByRole('alert')).toContainText('Incorrect username or password.');
+});
