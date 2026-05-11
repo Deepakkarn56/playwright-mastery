9833b852d40c97973e413f21ab138f7a8cffe210
Deepakkarn56 <karndeepakk@gmail.com>
2026-05-11 16:47:15 +0530

    Run playwright test in  headless mode

--- tests/recordatcursortest.spec.js
+++ tests/recordatcursortest.spec.js
@@ -1,5 +1,4 @@
-await page.getByRole('button', { name: 'Sign in', exact: true }).click();
-await expect(page.getByRole('button', { name: 'Sign in', exact: true })).toBeVisible();import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://github.com/Deepakkarn56');
