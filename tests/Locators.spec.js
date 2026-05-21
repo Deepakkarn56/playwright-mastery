062c86b93ac03111ddb0067e5218aa473c2fb125
Deepakkarn56 <karndeepakk@gmail.com>
2026-05-21 19:29:41 +0530

    Added successful login verification in Playwright test

--- tests/Locators.spec.js
+++ tests/Locators.spec.js
@@ -5,12 +5,16 @@ test('Locaters ',async({page})=>{
     await page.goto('https://www.saucedemo.com/')
 
     // locate id web element & user id - used Id property
-    await page.locator('id=user-name').fill('standard_user');
+    await page.locator('#user-name').fill('standard_user');
 
     // locate password  web element & fill password - by using css selector
     await page.locator('#password').fill('secret_sauce');
 
     // locate login button & perform click action - by using XPATH 
     await page.locator("//input[@id='login-button']").click()
+
+    // verify successful login
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    await expect(page.locator('.title')).toHaveText('Products'); 
  
-})
+}) 
