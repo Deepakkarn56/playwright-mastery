92243857ba83cad875e06b560586a0637223ede1
Deepakkarn56 <karndeepakk@gmail.com>
2026-05-22 09:27:49 +0530

    Implemented multiple locators in Playwright

---
+++ day 01-playwright-overview/Notess.md
@@ -0,0 +1,10 @@
+Locating web element on the web pages
+1 property (id ,name, etc)
+2 css  selector
+3 xpath
+
+Locate single web element 
+
+await page.locater('selector).click
+
+*preferd way to locate a single  element and click 
---
+++ tests/LocateMultipleElement.spec.js
@@ -0,0 +1,37 @@
+import {test,expect } from '@playwright/test';
+
+test('Locaters',async({page})=>{
+    // open url 
+    await page.goto('https://www.saucedemo.com/')
+
+    // enter username
+    await page.fill('#user-name','standard_user');
+
+    // enter password 
+    await page.locater('password').fill('secret_sauce');
+
+    // locate login button and perform click action  by using XPATH
+    await page.locator("//input[@id='login-button']").click();
+
+    // verify successful login
+    await expect (page).ttoHaveURL('/inventory.html'); // verify url 
+
+    // find list of all products
+ 
+    const products =  await page.$$ ('.inventory_item_name ')
+
+    // count products  and print
+      console.log ('Total no of products:', productNames.length);
+
+    //   loog through each element and print the product name
+
+    for (const product of productsNames){
+        const name = await product.textContent();  //each element is an element handle
+        // textContent() is an async fnc,that queries the element in the browser so you must await it 
+        console.log (name)
+
+    }
+
+
+                                                           
+})
