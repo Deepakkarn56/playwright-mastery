20befb50c018e2fea69378e039329f6d33b7edcd
Deepakkarn56 <karndeepakk@gmail.com>
2026-05-22 21:39:24 +0530

    test: add cart and product description assertions

--- tests/BuiltIn_Locators.spec.js
+++ tests/BuiltIn_Locators.spec.js
@@ -2,10 +2,10 @@ import {test,expect} from '@playwright/test';
 
 test('Built-In Locators-SauceDemo', async({page})=>{
 
-    // open sauce demo website
+    //   getbyplaceholder  open sauce demo website
     await page.goto('https://www.saucedemo.com/')
 
-    // enter user name
+    //   getbyplaceholder enter user name
    await  page.getByPlaceholder('Username').fill('standard_user')
 
 //    enter password
@@ -16,6 +16,15 @@ test('Built-In Locators-SauceDemo', async({page})=>{
 
 //    Assertion
   await expect(page).toHaveURL(/inventory.html/)
+
+//   get by text verify product visbility on inventry page
+  await  expect (page.getByText('Products')).toBeVisible()
+
+//    getByRole() add product  sauce lab backpack to cart
+await page.getByRole('button',{name: 'Add to cart'}).first.click
+
+// get by all text = click on sauce lab backpack product to view decription
+await page.getByAltText('Sauce Labs Backpack').click()
 
 
 })
