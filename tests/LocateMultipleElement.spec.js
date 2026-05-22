e632b7d3f5ac1ee26a425de09aa511489780d369
Deepakkarn56 <karndeepakk@gmail.com>
2026-05-22 10:06:02 +0530

    Implemented locator approach for multiple web elements

--- tests/LocateMultipleElement.spec.js
+++ tests/LocateMultipleElement.spec.js
@@ -16,22 +16,37 @@ test('Locaters',async({page})=>{
     // verify successful login
     await expect (page).ttoHaveURL('/inventory.html'); // verify url 
 
-    // find list of all products
+    // ---------First Approch 
+
+    // // find list of all products
  
-    const products =  await page.$$ ('.inventory_item_name ')
+    // const products =  await page.$$ ('.inventory_item_name ')
 
-    // count products  and print
-      console.log ('Total no of products:', productNames.length);
+    // // count products  and print
+    //   console.log ('Total no of products:', productNames.length);
 
-    //   loog through each element and print the product name
+    // //   loog through each element and print the product name
 
-    for (const product of productsNames){
-        const name = await product.textContent();  //each element is an element handle
-        // textContent() is an async fnc,that queries the element in the browser so you must await it 
-        console.log (name)
+    // for (const product of productsNames){
+    //     const name = await product.textContent();  //each element is an element handle
+    //     // textContent() is an async fnc,that queries the element in the browser so you must await it 
+    //     console.log (name)
 
-    }
+    // }
 
+    
+      // ---------Second Approch    RECOMMENDED APPROCH  
+      
+    //   locate all product names 
+    const productNames = page.locator('.inventory_item_name');
 
-                                                           
+    // get total count 
+    const count = await productNames.count ()
+    console.log ('Total no of products:', count)
+
+    // print each product name
+    for (let i =0; i<count ; i++){
+       const name = await productNames.nth(i).textContent();
+       console.log(name)
+    } 
 })
