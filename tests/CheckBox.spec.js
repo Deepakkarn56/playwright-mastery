0d803482ab80db0c564c03f69b6ddf000d979368
Deepakkarn56 <karndeepakk@gmail.com>
2026-06-01 21:19:39 +0530

    test: automate multiple checkbox selection using loop

--- tests/CheckBox.spec.js
+++ tests/CheckBox.spec.js
@@ -9,22 +9,33 @@ test("CheckBox", async ({ page }) => {
   const mondayCheckbox = page.locator("#monday");
   const tuesdayCheckbox = page.locator("#tuesday");
 
-//   await sundayCheckbox.check(); //check / select sunday checkbox
+  //   await sundayCheckbox.check(); //check / select sunday checkbox
 
   //    verify sunday checkbox is selected
-//   await expect(sundayCheckbox).toBeChecked();
+  //   await expect(sundayCheckbox).toBeChecked();
 
-    //  store allcheckbox in an array . This will help us use loops instead repeating code
+  //  store allcheckbox in an array . This will help us use loops instead repeating code
 
-    const checkboxes = [sundayCheckbox, mondayCheckbox, tuesdayCheckbox];
+  const checkboxes = [sundayCheckbox, mondayCheckbox, tuesdayCheckbox];
 
-    // for loop-  select all checkbox 
+  // for loop-  select all checkbox
 
-    for(const checkbox of checkboxes){
-        // check (select ) the checkbox
-        await checkbox.check();
+  for (const checkbox of checkboxes) {
+    // check (select ) the checkbox
+    await checkbox.check();
 
-        // verify checkbox is checked
-        expect(checkbox).toBeChecked();
+    // verify checkbox is checked
+    expect(checkbox).toBeChecked();
+  }
+
+  //   for loop + if condition - uncheck checkboxes
+  for (const checkbox of checkboxes) {
+    const ischecked = await checkbox.isChecked(); // true or false
+    // if condition
+    // only uncheck if checkbox is already unchecked
+    if (ischecked) {
+      await checkbox.uncheck();
     }
+  }
+  
 });
