44f73f81925c4197f032a5d7e23e8a430f28e1e5
Deepakkarn56 <karndeepakk@gmail.com>
2026-06-03 17:45:31 +0530

    test: verify dropdown options using for loop and flag assertion

--- tests/MultiSelectDropdown.spec.js
+++ tests/MultiSelectDropdown.spec.js
@@ -1,33 +1,32 @@
 import { test, expect } from "@playwright/test";
 
 test("MultipleSelectDropdown", async ({ page }) => {
-  //   open url
+  // open url
   await page.goto("https://testpages.eviltester.com/pages/forms/html-form/");
 
   // locate multi select dropdown
-
   const multiSelect = page.locator('select[name="multipleselect[]"]');
 
   // ways to select options from multi select dropdown
 
   // 1. by using value
-  //   await multiSelect.selectOption([{value:'ms1'},{value:'ms3'}]);
+  // await multiSelect.selectOption([{value:'ms1'},{value:'ms3'}]);
 
-  //   2. by using label/ visible text
+  // 2. by using label/ visible text
   await multiSelect.selectOption([
     { label: "Selection Item 2" },
     { label: "Selection Item 4" },
   ]);
 
-  //   verify if items are selected or not
+  // verify if items are selected or not
   await expect(multiSelect.locator("option:checked")).toContainText([
     "Selection Item 2",
     "Selection Item 4",
   ]);
 
-  await page.waitForTimeout(5000); //pause for demo purpose
+  await page.waitForTimeout(5000); // pause for demo purpose
 
-  //   Assertion / validation
+  // Assertion / validation
   // 1 verify dropdown is multiple select
   await expect(multiSelect).toHaveAttribute("multiple");
 
@@ -35,16 +34,41 @@ test("MultipleSelectDropdown", async ({ page }) => {
   const options = multiSelect.locator("option");
   await expect(options).toHaveCount(4);
 
-  //  3 verify  if items are selected or not
+  // 3 verify if items are selected or not
   await expect(multiSelect.locator("option:checked")).toContainText([
-    'Selection Item 2',
-    'Selection Item 4',
+    "Selection Item 2",
+    "Selection Item 4",
   ]);
 
   await expect(multiSelect.locator("option:checked")).not.toContainText([
-    'Selection Item 3',
-    'Selection Item 1',
+    "Selection Item 3",
+    "Selection Item 1",
   ]);
 
   await expect(options).toHaveCount(4);
+
+  // get all options element from dropdown as an array
+  const optionslist = await options.all();
+
+  // create a flag variable to track if the value is found
+  let ispresent = false;
+
+  // loop through each option one by one
+  for (const option of optionslist) {
+    // read the visible text of the current option
+    const optionText = await option.textContent(); 
+
+    if (optionText?.trim() === "Selection Item 2") {
+     
+
+      ispresent = true;
+
+      //   exit the loop because desired value is found
+      break;
+    }
+  }
+
+  //   final assertion to verify that the desired value /option is found in the dropdown
+
+  expect(ispresent).toBeTruthy();
 });
