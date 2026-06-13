dfeb21ca93aa31aaae76b08a1f5e829112171e17
Deepakkarn56 <karndeepakk@gmail.com>
2026-06-13 19:55:13 +0530

    test: add job title dropdown interaction and option validation on PIM page

--- tests/HiddenDropdown.spec.js
+++ tests/HiddenDropdown.spec.js
@@ -1,17 +1,46 @@
-import { test, expect } from "@playwright/test";
+import{test,expect} from '@playwright/test'
 
-test("HiddenDropdown", async ({ page }) => {
-  // open url
-  await page.goto(
-    "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
-  );
+test('HiddenDropdown',async({page})=>{
 
-  // locate username input box  and type username
-  await page.locator("[name='username']").fill("Admin");
+    //open OrangeHRM url
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
-  // locate password input box and enter password
-  await page.locator("[name='password']").fill("admin123");
+    //locate username input box and type user name
+    await page.locator("[name='username']").fill("Admin")
 
-  // locate login button and perform click action
-  await page.getByRole("button", { name: "Login" }).click();
-});
+    //locate password input box and enter password 
+    await page.locator("[name='password']").fill("admin123")
+
+    //locate login button and perform click action
+    await page.getByRole('button',{name:'Login'}).click()
+
+    //locate PIM link and perform click action
+    await page.getByRole('link',{name:'PIM'}).click()
+
+    // --------------------------------------Hidden dropdown----------------------------
+    //locate job title dropdown and click on it
+    await page.locator('.oxd-select-text-input').nth(2).click()
+
+    //waitForSelector waits untill the given element appears or becomes visible
+    await page.waitForSelector('.oxd-select-dropdown[role="listbox"]',{state:'visible'})
+
+    //locate all dropdown options
+    const options = page.locator('.oxd-select-option span')
+
+    //count() returns the total number of matching element 
+    const count = await options.count()
+    //print on terminal
+    console.log("Total options:",count)
+
+    //loop through each dropdown option using index
+    for (let i=0; i<count;i++)
+    {
+        //innerText() reads only the visible text shown to the user
+        const text = await options.nth(i).innerText()
+        console.log(text)
+    }
+
+    await page.locator('.oxd-select-option span',{hasText:'Head of Support'}).click()
+
+    await page.waitForTimeout(5000)
+})
