30af13065858b7dddf05f461638b90cdaa10cfe0
Deepakkarn56 <karndeepakk@gmail.com>
2026-06-20 08:42:50 +0530

    test: add Playwright iframe test

--- tests/iFrame.spec.js
+++ tests/iFrame.spec.js
@@ -1,15 +1,22 @@
-import {test,expect} from '@playwright/test'
+import { test, expect } from '@playwright/test';
 
-test('iFrame', async({page})=>{
-    
-    // open url 
-    await page.goto("http://127.0.0.1:5500/DemoHtmlDosc/index.html")
+test('iFrame', async ({ page }) => {
 
-    // count total iframe
+    // Open URL
+    await page.goto('http://127.0.0.1:5500/DemoHtmlDosc/index.html');
 
-    const allFrame =  page.frames()
+    // Count total frames
+    const allFrames = page.frames();
+    console.log('Total number of frames:', allFrames.length);
 
-    console.log("Total number of frames:", allFrame.length)
+    // Access first iframe (Form Practice)
+    const leftFrame = page.frameLocator('iframe').first();
 
+    // Fill form fields
+    await leftFrame.getByLabel('Name:').fill('Deepak');
+    await leftFrame.getByLabel('Email:').fill('deepak@example.com');
 
-})
+    // Click submit
+    await leftFrame.getByRole('button', { name: 'Submit' }).click();
+
+});
